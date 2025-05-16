a81174fddcdd61c76b01b37d5f3cdc201f084aad
Omole Oluwadamilare Daniel <147058687+ODaniel235@users.noreply.github.com>
2025-05-16 14:22:07 +0100

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 import cors from "cors";
+import Cron from "node-cron";
 import router from "./src/routes/router.js";
 app.use(cookieParser());
 dotenv.config();
@@ -20,6 +21,30 @@ app.options("*", cors(corsOptions)); // Preflight OPTIONS request handling
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api", router);
+
+Cron.schedule(
+  "*/30 * * * * *",
+  async () => {
+    const WEBSITE_URL =
+      process.env.RENDER_URL ||
+      `http://localhost:${process.env.PORT || PORT}/keep-alive`;
+    try {
+      const startTime = Date.now();
+      const response = await fetch(WEBSITE_URL, {
+        headers: { "User-Agent": "Keep-Alive" },
+      });
+      if (!response.ok) throw new Error(`HTTP ${response.status}`);
+      console.log(
+        `[Keep-Alive] Visited ${WEBSITE_URL} and it is up and running`
+      );
+      logger.info(`Time took to ping ${Date.now() - startTime}ms`);
+    } catch (error) {
+      console.log(error);
+      console.error(`[Fatal-Error] : [Keep-Alive] Failed: ${error.message}`);
+    }
+  },
+  { timezone: "UTC" }
+);
 const __dirname = path.resolve();
 const PORT = 8080;
 // Enable CORS for your frontend (localhost:8000)
